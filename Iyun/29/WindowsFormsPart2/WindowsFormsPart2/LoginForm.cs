fa6f1a485a6aa949276f93c4170ffad5e1bb1b1d
Orkhan Farajov <orkhankhf@gmail.com>
2023-06-29 23:17:24 +0400

    Windows Forms Part 2

---
+++ Iyun/29/WindowsFormsPart2/WindowsFormsPart2/LoginForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsPart2
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginMethod(object sender, EventArgs e)
+        {
+            string username = usernameTextBox.Text;
+            string password = passwordTextBox.Text;
+
+            User foundUser = VirtualDatabase.Users.Find(m => m.Username == username && m.Password == password);
+
+            if (foundUser != null)
+            {
+                HomePage homePage = new HomePage(foundUser);
+                homePage.Show();
+            }
+            else
+            {
+                MessageBox.Show("İstifadəçi məlumatları yanlışdır!", "Xəta!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void TextboxEnterColor(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            textBox.BackColor = Color.Yellow;
+        }
+
+        private void TextboxLeaveColor(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            textBox.BackColor = Color.White;
+        }
+    }
+}
